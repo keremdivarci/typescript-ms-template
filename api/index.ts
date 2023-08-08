c794ad858afb611046270f7bfe2f7a10ed7bd24e
qraxiss <qraxiss@gmail.com>
2023-08-08 15:41:44 +0300

    origin fix.

--- api/index.ts
+++ api/index.ts
@@ -24,13 +24,12 @@ app.use(Morgan('dev'))
 app.use(Express.json({ limit: '10mb' }))
 
 app.use((req, res, next) => {
-    if (req.headers.origin) {
-        res.header('Access-Control-Allow-Origin', req.headers.origin)
-    }
-
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH')
+    // allow all origins
+    res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Credentials', 'true')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-PINGOTHER')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH, HEAD, OPTIONS')
+    res.header('Access-Control-Expose-Headers', 'Set-Cookie')
     next()
 })
 
