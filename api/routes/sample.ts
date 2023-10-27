18430eb3e4d66fb5681f5f7a571116a2e1634c09
qraxiss <qraxiss@gmail.com>
2023-10-27 17:26:12 +0300

    deleted access func

--- api/routes/sample.ts
+++ api/routes/sample.ts
@@ -2,14 +2,12 @@ import { Router } from 'express'
 
 import { SampleController } from '../controllers/sample'
 
-import { requireAcces } from '../middlewares/access'
-
 const sampleRouter = Router()
 
-sampleRouter.get('/query', requireAcces(['sample', 'read']), SampleController.getSample)
-sampleRouter.get('/query/all/', requireAcces(['sample', 'read', 'all']), SampleController.getSamples)
-sampleRouter.post('/', requireAcces(['sample', 'create']), SampleController.createSample)
-sampleRouter.put('/', requireAcces(['sample', 'update']), SampleController.updateSample)
-sampleRouter.delete('/', requireAcces(['sample', 'delete']), SampleController.deleteSample)
+sampleRouter.get('/query', SampleController.getSample)
+sampleRouter.get('/query/all/', SampleController.getSamples)
+sampleRouter.post('/', SampleController.createSample)
+sampleRouter.put('/', SampleController.updateSample)
+sampleRouter.delete('/', SampleController.deleteSample)
 
 export { sampleRouter }
